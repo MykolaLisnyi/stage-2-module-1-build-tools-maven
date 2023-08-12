1b13c6d36bf84129582a6c84035129e08be3a669
Mykola Lisnyi <lesnoy.nikolay@gmail.com>
2023-08-12 19:38:02 +0300

    2

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,10 +1,7 @@
 package com.epam.demo;
 
 import com.epam.utils.StringUtils;
-
 import java.util.List;
-
-import static com.epam.utils.StringUtils.isPositiveNumber;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,6 +4,6 @@ import static org.apache.commons.lang3.StringUtils.isNumeric;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return isNumeric(str) && Integer.parseInt(str) > 0;
+        return isNumeric(str);
     }
 }
